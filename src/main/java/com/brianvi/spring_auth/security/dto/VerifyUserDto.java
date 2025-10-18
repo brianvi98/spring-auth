6843364b11cd237cb802a49e194e6cdf71aa5679
brianvi98 <brianvi98@gmail.com>
2025-10-18 15:00:16 -0700

    implement authentication service

--- src/main/java/com/brianvi/spring_auth/security/dto/VerifyUserDto.java
+++ src/main/java/com/brianvi/spring_auth/security/dto/VerifyUserDto.java
@@ -1,5 +1,10 @@
 package com.brianvi.spring_auth.security.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class VerifyUserDto {
     private String email;
     private String verificationCode;
