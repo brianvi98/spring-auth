e9413a863012324c2e5da092c25b7cbda30fcca1
brianvi98 <brianvi98@gmail.com>
2025-10-18 15:34:30 -0700

    implemented routes in authentication controller

---
+++ src/main/java/com/brianvi/spring_auth/security/controller/AuthenticationController.java
@@ -0,0 +1,60 @@
+package com.brianvi.spring_auth.security.controller;
+
+import com.brianvi.spring_auth.security.dto.LoginUserDto;
+import com.brianvi.spring_auth.security.dto.RegisterUserDto;
+import com.brianvi.spring_auth.security.dto.VerifyUserDto;
+import com.brianvi.spring_auth.security.response.LoginResponse;
+import com.brianvi.spring_auth.security.service.AuthenticationService;
+import com.brianvi.spring_auth.security.service.JwtService;
+import com.brianvi.spring_auth.user.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signUp(registerUserDto);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+        LoginResponse loginResponse = new LoginResponse(jwtToken, jwtService.getExpirationTime());
+        return ResponseEntity.ok(loginResponse);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<?> verify(@RequestBody VerifyUserDto verifyUserDto) {
+        try {
+            authenticationService.verifyUser(verifyUserDto);
+            return ResponseEntity.ok("Account successfully verified");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/resend")
+    public ResponseEntity<?> resendVerificationCode(@RequestParam String email ) {
+        try {
+            authenticationService.resendVerificationCode(email);
+            return ResponseEntity.ok("Verification code sent");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+
+}
---
+++ src/main/java/com/brianvi/spring_auth/security/response/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.brianvi.spring_auth.security.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginResponse {
+    private String token;
+    private long expiresIn;
+
+    public LoginResponse(String token, long expiresIn) {
+        this.token = token;
+        this.expiresIn = expiresIn;
+    }
+
+
+}
