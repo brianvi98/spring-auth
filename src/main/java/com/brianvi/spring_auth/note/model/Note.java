fc5d4eef2cbe521750819c9b986938b8cd3adab3
brianvi98 <brianvi98@gmail.com>
2025-10-29 17:32:08 -0700

    add Note model and repo

---
+++ src/main/java/com/brianvi/spring_auth/note/model/Note.java
@@ -0,0 +1,40 @@
+package com.brianvi.spring_auth.note.model;
+
+import com.brianvi.spring_auth.user.model.User;
+import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name="notes")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 1000)
+    private String content;
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
